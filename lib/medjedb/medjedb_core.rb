086c7ec287c0aa43d9f308419562d26e30585926
TMogi <mogi@val.co.jp>
2018-05-08 19:46:52 +0900

    CSVファイルからデータを読み込んでDBに登録する仕組みを作成

---
+++ lib/medjedb/medjedb_core.rb
@@ -0,0 +1,49 @@
+require_relative 'db_core.rb'
+require_relative 'read_csv.rb'
+
+class MedjedbCore
+  def initialize(db_path)
+    @db_core = DbCore.new(db_path)
+  end
+
+  # テーブルを空の状態に初期化
+  def init_table()
+    @db_core.create_data_master_table
+    @db_core.create_eki_code_index_table
+  end
+
+  # CSVからデータマスタに登録
+  def load_data_master(csv_path)
+    ReadCsv.each_row(csv_path) do |row|
+      unless row.header_row?
+        # キーはカラム0
+        # データは対象の行をハッシュで登録
+        DataMaster.add(row[0], row.to_hash)
+      end
+    end
+  end
+
+  # CSVから駅データテーブルに登録
+  def load_eki_code_index_data(csv_path)
+    ReadCsv.each_row(csv_path) do |row|
+      unless row.header_row?
+        # キーはカラム0
+        # 対象のマスタIDはカラム1
+        EkiCodeIdx.add(row[0], row[1])
+      end
+    end
+  end
+
+  # 駅コードを全て取得
+  def find_target_eki_codes()
+    EkiCodeIdx.get_all_index
+  end
+
+  # 対象キーのデータを取得する(Hash)
+  def find_information(eki_id)
+    dm = DataMaster.find_data(eki_id)
+
+    return {} if dm.nil?
+    return dm.get_hash()
+  end
+end
---
+++ spec/mejedb_core_spec.rb
@@ -0,0 +1,32 @@
+require 'spec_helper.rb'
+require_relative '../lib/medjedb/medjedb_core.rb'
+
+RSpec.describe MedjedbCore do
+  before :all do
+    # データ初期化
+    @db = MedjedbCore.new("spec/test_db")
+    @db.init_table
+    @db.load_data_master("spec/test_master.csv")
+    @db.load_eki_code_index_data("spec/test_eki.csv")
+  end
+  
+  context "駅データテスト" do
+    it "登録されているデータを全て読み込む" do
+      expect(@db.find_target_eki_codes().length()).to eq(3)
+    end
+  end
+
+  context "マスタテスト" do
+    it "キーが存在するときはHash形式でデータを返す" do
+      data = @db.find_information(0)
+      expect(data["sample0"]).to eq("test0")
+      expect(data["sample1"]).to eq("test1")
+      expect(data["sample2"]).to eq("test2")
+      expect(data["sample3"]).to eq("test3")
+    end
+
+    it "キーが存在しない場合は空のHashを返す" do
+      expect(@db.find_information(99).empty?).to be_truthy
+    end
+  end
+end
